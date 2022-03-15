c81b12db21f61e5c902fb6c6dfaf707d0e4cabb5
hss <1395643963@qq.com>
2022-03-15 12:01:52 +0000

    添加eureka服务配置中心模块；给eureka服务注册中心模块注册2个服务，步骤：1.给pom通过添加g、a、v坐标导入jar；2.修改配置文件：设置注册服务名称、将模块注册进eureka、设置访问路径。进行bean对象的设置；3.给主启动类开启eureka的注解；
    git-svn-id: https://localhost/svn/helloSpringCloud/trunk@17 8c15abe5-7d27-f146-b0b1-1da5c01357ed

--- HelloSpringCloud2022/cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/PaymentMain8001.java
+++ HelloSpringCloud2022/cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/PaymentMain8001.java
@@ -2,7 +2,9 @@ package com.atguigu.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient //表示将该模块，注册进 Eureka 的注解
 @SpringBootApplication
 public class PaymentMain8001 {
     public static void main(String[] args) {
