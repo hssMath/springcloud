cb58852878b5025f374c906b47ac4b8225b2e76a
hss <1395643963@qq.com>
2022-03-15 12:59:48 +0000

    eureka的集群配置：使得 cloud-eureka-server7001 和 cloud-eureka-server7002，实现集群，关联关系：服务调用地址互相交叉；子模块注册进父工程。
    git-svn-id: https://localhost/svn/helloSpringCloud/trunk@18 8c15abe5-7d27-f146-b0b1-1da5c01357ed

---
+++ HelloSpringCloud2022/cloud-eureka-server7002/src/main/java/com/atguigu/springcloud/EurekaMain7002.java
@@ -0,0 +1,16 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 主启动类用于启动 Eureka 的服务注册中心，管理服务之间的配置、登记、注册、调用
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaMain7002 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7002.class,args);
+    }
+}
