977d58b65add2239328c94eafe95ad9a820918ee
hss <1395643963@qq.com>
2022-03-29 09:10:20 +0000

    使用 nacos 作为windows 环境下的单机版的服务配置中心，实现配置中心的功能。 nacos等于服务注册中心+服务配置中心+服务总线，支持CP、AP的切换。是一个更易于构建云原生应用的动态服务发现、配置管理和服务管理平台。 1、Nacos 同 spring cloud config一样，在项目初始化时，要保证先从配置中心进行配置拉取，拉取配置之后，才能保证项目的正常启动。springboot 中配置文件的加载是存在优先级顺序的，bootstrap 优先级高于 application。通常情况下，使用 bootstrap 设置微服务的通用不可变的配置，application 用于可变化环境的配置。 2、通过 yml 配置文件，将 3377 微服务注册进 Nacos,并根据官网给出的配置文件格式：${spring.application.name}-${spring.profile.active}.${spring.cloud.nacos.config.file-extension} 进行关联，项目运行之后可实现配置文件的自动刷新（自动刷新需要在控制器类加入 @RefreshScope 注解使当前类下的配置支持 Nacos 的动态刷新功能）：配置文件查找顺序：先找命名空间，再找分组，最后找指定的文件。
    git-svn-id: https://localhost/svn/helloSpringCloud/trunk@57 8c15abe5-7d27-f146-b0b1-1da5c01357ed

---
+++ HelloSpringCloudAlibba2022/cloudalibaba-config-nacos-client3377/src/main/java/com/atguigu/springcloudalibaba/ctroller/ConfigClientController.java
@@ -0,0 +1,18 @@
+package com.atguigu.springcloudalibaba.ctroller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope //在控制器类加入@RefreshScope 注解使当前类下的配置支持 Nacos 的动态刷新功能。
+public class ConfigClientController {
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/config/info")
+    public String getConfigInfo() {
+        return configInfo;
+    }
+}
